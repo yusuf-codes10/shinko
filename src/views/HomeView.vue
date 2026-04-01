429bb9dce9d827dabad07c49864eee870d72a858
Yusef Codes <youcefcopy@gmail.com>
2026-04-01 18:27:23 +0100

    color pallete

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,10 @@
 </script>
 
 <template>
-  <div class="flex justify-center items-center h-screen bg-yellow-400">This is the Home vue</div>
+  <div class="flex justify-center items-center h-screen bg-yellow-400">
+    This is the Home vue
+    <button>Create a new Project</button>
+  </div>
 </template>
 
 <style scoped>
