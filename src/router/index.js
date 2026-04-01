8d68098db5e0ce77792ee992e49c9ca657077447
Yusef Codes <youcefcopy@gmail.com>
2026-04-01 18:11:51 +0100

    back to shinko

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-screen bg-yellow-400">This is the Home vue</div>
+</template>
+
+<style scoped>
+</style>
