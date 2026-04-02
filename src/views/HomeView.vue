2cc278b0428116c415cc94277120e039d41b51b1
Yusef Codes <youcefcopy@gmail.com>
2026-04-02 17:09:34 +0100

    that is it for the styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,8 @@
 <template>
   <div class="flex flex-col justify-center items-center h-screen">
     <h1 class="text-6xl md:text-9xl my-2 text-extrabold">shinko</h1>
-    <button class="text-red-500 bg-green-400 px-1 py-2 rounded-xl cursor-pointer">
-      Create a new Project ->
+    <button class="text-red-500 bg-green-400 px-1 py-2 rounded-sm cursor-pointer">
+      Create New Project &rarr;
     </button>
   </div>
 </template>
