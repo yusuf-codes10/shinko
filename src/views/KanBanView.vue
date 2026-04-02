9bb91cd1dcc9bb33383860f1878c0b0b5abcddce
Yusef Codes <youcefcopy@gmail.com>
2026-04-02 18:39:57 +0100

    async/await synax is cleaner

--- src/views/KanBanView.vue
+++ src/views/KanBanView.vue
@@ -1,24 +1,25 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 
-const data = ref([])
+const projects = ref([])
 
 // fetch the data with the onMounted lifecycle hook
-onMounted(() => {
+onMounted(async () => {
   const endpoint = 'http://localhost:3000/projects'
-  fetch(endpoint)
-    .then((res) => res.json())
-    .then((d) => (data.value = d))
-    .catch((err) => console.log(err.message))
+  try {
+    const res = await fetch(endpoint)
+    const data = await res.json()
+    projects.value = data
+  } catch (err) {
+    console.log(err.message)
+  }
 })
-
-console.log(data.value)
 </script>
 
 <template>
   <div>
     <h1 class="text-7xl">Is this woking?</h1>
-    <div class="h-screen bg-amber-600" v-for="project in data" :key="project.id">
+    <div class="h-screen bg-amber-600" v-for="project in projects" :key="project.id">
       <h1>Name: {{ project.name }}</h1>
       <h2>Status: {{ project.status }}</h2>
     </div>
