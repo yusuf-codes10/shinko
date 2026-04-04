b8a9df7cf5238e2811fc0bdae6c76c1af6daeab4
Yusef Codes <youcefcopy@gmail.com>
2026-04-04 17:18:39 +0100

    defining the add something function, a post req and a headers attr

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,8 +1,32 @@
 <script setup>
+const addSomething = async (project) => {
+  const endpoint = 'http://localhost:3000/projects'
+
+  try {
+    const res = await fetch(endpoint, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application.json' },
+      body: JSON.stringify(project),
+    })
+    // we have the res
+    const data = await res.json()
+    console.log(data)
+  } catch (err) {
+    console.log(err.nessage)
+  }
+}
 </script>
 
 <template>
-  <div></div>
+  <div>
+    This is the create view
+    <div>
+      add a new project:
+      <label for="project">Project name</label>
+      <input type="text" name="" id="project" v-model="project" />
+      <button @click="addSomething(project)"></button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
