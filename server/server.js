3d58a658d98fe4aadb93bc2970309db881934515
Yusef Codes <youcefcopy@gmail.com>
2026-04-19 15:49:13 +0100

    using the logger on the app level | global

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from "express";
+import logger from "./middlewares/logger.js";
 
 const app = express();
+app.use(logger);
 
 app.listen(8080, () => {
   console.log(`server's running on 8080`);
