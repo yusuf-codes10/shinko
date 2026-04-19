7301bd06ed4483f8d2a79795863c776df4060634
Yusef Codes <youcefcopy@gmail.com>
2026-04-19 16:04:49 +0100

    cannot get unifined id?

--- server/router/router.js
+++ server/router/router.js
@@ -28,6 +28,13 @@ router.get("/:id", (req, res) => {
 
   // find that on the data
   const foundData = data.find((item) => item.id === id);
+
+  //in case not found
+  if (!foundData) {
+    return res.status(404).json({ msg: "User not found" });
+  }
+
+  res.status(200).json(foundData);
 });
 
 export default router;
