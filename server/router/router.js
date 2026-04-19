d302a24b4ef7102425d65b5c4903c5b1ab1dee44
Yusef Codes <youcefcopy@gmail.com>
2026-04-19 15:51:05 +0100

    hardcoding some data to ensure the get req is working

--- server/router/router.js
+++ server/router/router.js
@@ -2,4 +2,19 @@ import express from "express";
 
 const router = express.Router();
 
+const data = [
+  {
+    id: 1,
+    name: "Jessica",
+  },
+  {
+    id: 2,
+    name: "Alice",
+  },
+  {
+    id: 3,
+    name: "Kim",
+  },
+];
+
 export default router;
