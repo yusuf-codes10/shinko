f231a98d85df55086c36dbbb2812e46632ece787
Yusef Codes <youcefcopy@gmail.com>
2026-04-19 16:07:14 +0100

    this is a better mw of course

--- server/middlewares/logger.js
+++ server/middlewares/logger.js
@@ -1,5 +1,7 @@
 const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.protocol}//:${req.get("host")}`);
+  console.log(
+    `${req.method} ${req.protocol}//:${req.get("host")}${req.originalUrl}`,
+  );
   next();
 };
 
