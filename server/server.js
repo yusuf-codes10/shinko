55193e46e684de76f7f02795d204d97247cb5fe9
Yusef Codes <youcefcopy@gmail.com>
2026-04-19 16:49:07 +0100

    fixing the cors issue

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 import express from "express";
 import logger from "./middlewares/logger.js";
 import router from "./router/router.js";
+import cors from "cors";
 
 const app = express();
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(logger);
 app.use("/api", router);
 
