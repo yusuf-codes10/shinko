f07e2d738ea7fbe45b9317e6b85d81a5ea33bf6c
Yusef Codes <youcefcopy@gmail.com>
2026-04-20 09:57:55 +0100

    first of all, we should create a user account

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
-import '@/components/NavBar.vue'
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
   <div>
     <NavBar />
-    <h2>Waht</h2>
     <RouterView />
   </div>
 </template>
--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -2,9 +2,9 @@
 </script>
 
 <template>
-  <nav class="flex justify-between items-center w-full bg-red-500">
-    <h1>Shinko</h1>
-    <button class="bg-blue-300 px-4 py-2">Login</button>
+  <nav class="flex justify-between items-center w-full">
+    <h1 class="text-3xl">Shinko</h1>
+    <button class="bg-blue-300 px-4 py-2 cursor-pointer rounded-2xl">Login</button>
   </nav>
 </template>
 
