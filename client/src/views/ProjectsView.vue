323d9e848b6df6f551ab64bdb8af2936ddfcd73e
Yusef Codes <youcefcopy@gmail.com>
2026-04-22 15:44:45 +0100

    get request on the vue side

--- client/src/views/ProjectsView.vue
+++ client/src/views/ProjectsView.vue
@@ -1,4 +1,23 @@
 <script setup>
+import { ref, onMounted } from 'vue'
+
+const projects = ref([])
+
+const getAllProjects = async () => {
+  const endpoint = 'http//:localhost:8080/api/projects'
+  try {
+    const response = await fetch(endpoint)
+    const data = await response.json()
+    console.log(data)
+    return data
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+onMounted(async () => {
+  projects.value = await getAllProjects()
+})
 </script>
 
 <template>
