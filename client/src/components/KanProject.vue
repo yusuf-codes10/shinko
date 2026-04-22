1bec3c5f348f68063ce617b0283618b898f5566c
Yusef Codes <youcefcopy@gmail.com>
2026-04-22 15:49:43 +0100

    the project component has been created

---
+++ client/src/components/KanProject.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+})
+</script>
+
+<template>
+  <div class="p-4 m-4 bg-blue-300">
+    <h1>{{ props.title }}</h1>
+    <p>{{ props.description }}</p>
+  </div>
+</template>
