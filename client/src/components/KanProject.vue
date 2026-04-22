779b44bbbaec54c4c7d6b57f97fe987b6e863b13
Yusef Codes <youcefcopy@gmail.com>
2026-04-22 16:19:09 +0100

    navigating to the tasks is working, all we need is to adjust the queries

--- client/src/components/KanProject.vue
+++ client/src/components/KanProject.vue
@@ -15,7 +15,7 @@ const props = defineProps({
 <template>
   <div class="p-4 m-4 bg-blue-300">
     <h1>
-      <RouterLink :to="{ name: 'Kanban', params: { id: props.id } }"></RouterLink> {{ props.name }}
+      <RouterLink :to="{ name: 'Kanban', params: { id: props.id } }">{{ props.name }}</RouterLink>
     </h1>
     <p>{{ props.description }}</p>
   </div>
