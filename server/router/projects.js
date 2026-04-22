f641784f5ecbc4e5a63002062c5d580b7f683622
Yusef Codes <youcefcopy@gmail.com>
2026-04-22 15:17:57 +0100

    a dummy array to test the get request

--- server/router/projects.js
+++ server/router/projects.js
@@ -2,3 +2,18 @@ import express from "express";
 import pool from "../db/pool.js";
 
 const projectsRouter = express.Router();
+
+const projectList = [
+  {
+    id: 1,
+    name: "pro 1",
+  },
+  {
+    id: 2,
+    name: "pro 2",
+  },
+];
+
+projectsRouter.get("/", (req, res) => {
+  res.status(200).json(projectList);
+});
