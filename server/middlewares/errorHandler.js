4ebc5e11fce7ef3eb35b9f003b07badeaa361541
Yusef Codes <youcefcopy@gmail.com>
2026-04-23 13:35:31 +0100

    never remvoe next from the custom error handler, otherwise express would fallback to its own default

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 // ! this is the cutom error handler
 const handleError = (err, req, res, next) => {
   console.error(err);
-
+  console.log(next);
   const statusCode = err.status || 500;
   const message = statusCode === 500 ? "Internal Server Error" : err.message;
   res.status(statusCode).json({ msg: message });
