6d7184a5d60c9e2fb1f2a2132b8cdf73be46cde3
Yusef Codes <youcefcopy@gmail.com>
2026-04-23 12:09:28 +0100

    a cutom error handler has been definied

---
+++ server/middlewares/errorHandler.js
@@ -0,0 +1,7 @@
+// ! this is the cutom error handler
+const handleError = (err, req, res, next) => {
+  res.status(err.status || 500).json({ msg: err.message });
+  next();
+};
+
+export default handleError;
