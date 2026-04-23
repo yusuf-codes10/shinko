b95efd241d893ec7d6a4d246d74c5e7cb295fd3d
Yusef Codes <youcefcopy@gmail.com>
2026-04-23 14:54:05 +0100

    using colors if the logger middleware

--- server/middlewares/logger.js
+++ server/middlewares/logger.js
@@ -1,6 +1,16 @@
+import colors from 'colors';
+
 const logger = (req, res, next) => {
+  const methodColors = {
+    GET: 'green',
+    POST: 'blue',
+    PUT: 'yellow',
+    DELETE: 'red'
+  }
+
+  const colorsList = methodColors[req.method] || white;
   console.log(
-    `${req.method} ${req.protocol}//:${req.get("host")}${req.originalUrl}`,
+    `${req.method} ${req.protocol}//:${req.get("host")}${req.originalUrl}`[colorsList],
   );
   next();
 };
