f9e104a0c6c0a778f489dcaebe846dc7275ccf37
Yusef Codes <youcefcopy@gmail.com>
2026-04-23 12:11:09 +0100

    using the custom MW in the app lvl

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import logger from "./middlewares/logger.js";
 import router from "./router/router.js";
 import projectsRouter from "./router/projects.js";
+import handleError from "./middlewares/errorHandler.js";
 import cors from "cors";
 
 const app = express();
@@ -14,6 +15,9 @@ app.use(logger);
 app.use(express.json()); //for reading the body
 app.use("/api/projects", projectsRouter);
 app.use("/api", router);
+
+// custom error handler
+app.use(handleError);
 
 app.listen(8080, () => {
   console.log(`server's running on 8080`);
