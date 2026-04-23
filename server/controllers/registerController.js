002ca61f09e947349a912b64d7384586fcc7fb8c
Yusef Codes <youcefcopy@gmail.com>
2026-04-23 15:22:59 +0100

    validating the username and checking for duplicates

--- server/controllers/registerController.js
+++ server/controllers/registerController.js
@@ -1,1 +1,15 @@
 // this is the register controller
+
+const handleUser = (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password)
+    return res.status(400).json({ msg: "username and password are required!" });
+
+  //   we have the username and password
+  // check for duplicates
+  const duplicate = "something in the db";
+
+  if (username === duplicate)
+    return res.status(400).json({ msg: "username already exists" });
+};
