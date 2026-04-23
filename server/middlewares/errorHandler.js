1309af2dbe8b6610d03edad40a9f83f8786a48c8
Yusef Codes <youcefcopy@gmail.com>
2026-04-23 12:50:59 +0100

    oops! forgot to not call next() in the error mw

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -1,7 +1,9 @@
 // ! this is the cutom error handler
 const handleError = (err, req, res, next) => {
-  res.status(err.status || 500).json({ msg: err.message });
-  next();
+  res
+    .status(err.status || 500)
+    .json({ msg: "Oops! it is over man, just give it up" });
+  // next();
 };
 
 export default handleError;
--- server/router/router.js
+++ server/router/router.js
@@ -24,13 +24,13 @@ router.get("/todo/:id", async (req, res, next) => {
   const projectId = req.params.id;
   try {
     const result = await pool.query(
-      "SELECT id, title, status FROM task WHERE status = 'todo' AND project_id = $1",
+      "SELECT id, titlus, status FROM task WHERE status = 'todo' AND project_id = $1",
       [projectId],
     );
     res.status(200).json(result.rows);
   } catch (error) {
     console.log(error.message);
-    res.status(500).json({ msg: "Database error" });
+    next(error);
   }
 });
 
