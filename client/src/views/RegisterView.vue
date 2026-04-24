5759d3d091cfeef73126fa9f742d76423e8e7462
Yusef Codes <youcefcopy@gmail.com>
2026-04-24 14:35:15 +0100

    adding some placeholders

--- client/src/views/RegisterView.vue
+++ client/src/views/RegisterView.vue
@@ -10,13 +10,25 @@ const email = ref('')
 <template>
   <div class="flex flex-col w-80 gap-2">
     <label for="email">Email</label>
-    <input type="email" id="email" class="bg-white" />
+    <input type="email" id="email" placeholder="example@email.com" class="bg-white" />
     <label for="username">Username</label>
-    <input type="text" id="username" class="bg-white" />
+    <input type="text" id="username" placeholder="johnDoe" class="bg-white" />
     <label for="password">Password</label>
-    <input type="password" id="password" minlength="8" class="bg-white" />
-    <label for="password">Password</label>
-    <input type="password" id="password" minlength="8" class="bg-white" />
+    <input
+      type="password"
+      id="password"
+      minlength="8"
+      placeholder="Password ..."
+      class="bg-white"
+    />
+    <label for="password">Confirm Password</label>
+    <input
+      type="password"
+      id="password"
+      minlength="8"
+      placeholder="Confirm Password"
+      class="bg-white"
+    />
     <div class="flex justify-between">
       <button class="bg-blue-300 cursor-pointer px-2 py rounded">Register</button>
       <button class="bg-blue-300 cursor-pointer px-2 py rounded">Cancel</button>
