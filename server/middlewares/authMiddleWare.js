a8dfb4bcd544a3b5da078d24d5e93068b9dd17d1
Yusef Codes <youcefcopy@gmail.com>
2026-04-24 16:43:23 +0100

    in case there is no token

--- server/middlewares/authMiddleWare.js
+++ server/middlewares/authMiddleWare.js
@@ -2,6 +2,8 @@ import jwt from "jsonwebtoken";
 
 const authMw = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) return res.status(401).json({ msg: "No token provided!" });
 };
 
 export default authMw;
