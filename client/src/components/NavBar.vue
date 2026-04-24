8a18a7108009e0ad7f8929c8a1d1594f7f614640
Yusef Codes <youcefcopy@gmail.com>
2026-04-24 17:32:44 +0100

    grabbing the username succussefuly | we might need a fallback for that

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,20 +1,24 @@
 <script setup>
 import { jwtDecode } from 'jwt-decode'
-
+import { ref } from 'vue'
 const token = localStorage.getItem('token')
+
+const username = ref('')
 
 let decoded = null
 console.log(decoded)
 if (token) {
   decoded = jwtDecode(token)
   console.log(decoded.username)
+  username.value = decoded.username
 }
 </script>
 
 <template>
   <nav class="flex justify-between items-center w-full">
     <h1 class="text-3xl">Shinko</h1>
-    <button class="bg-blue-300 px-4 py-2 cursor-pointer rounded-2xl">Login</button>
+    <h2 v-if="token">{{ username }}</h2>
+    <button v-else class="bg-blue-300 px-4 py-2 cursor-pointer rounded-2xl">Login</button>
   </nav>
 </template>
 
