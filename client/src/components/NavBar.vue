fb6552cea0a154b6e6ec3ecaef5174fde51e6965
Yusef Codes <youcefcopy@gmail.com>
2026-04-24 17:20:09 +0100

    using jwt-decode to decode the token from the localStorage

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,4 +1,11 @@
 <script setup>
+import jwtDecode from 'jwt-decode'
+
+const token = localStorage.getItem('token')
+
+const decoded = jwtDecode(token)
+
+console.log(decoded.username)
 </script>
 
 <template>
