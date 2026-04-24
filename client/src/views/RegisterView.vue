edf95a041f71d5e59bd732ba1b6661920d81564c
Yusef Codes <youcefcopy@gmail.com>
2026-04-24 16:07:20 +0100

    conditional rendering for the signup/login

--- client/src/views/RegisterView.vue
+++ client/src/views/RegisterView.vue
@@ -5,6 +5,7 @@ const username = ref('')
 const password = ref('')
 const verifiedPassword = ref('')
 const email = ref('')
+const isLogin = ref(true)
 
 const createUser = async () => {
   const endpoint = `http://localhost:8080/register`
@@ -35,43 +36,62 @@ const cancelForm = () => {
   verifiedPassword.value = ''
   email.value = ''
 }
+
+const toggleLogin = () => {
+  isLogin.value = !isLogin.value
+}
 </script>
 
 <template>
-  <div class="flex flex-col w-80 gap-2">
-    <label for="email">Email</label>
-    <input
-      type="email"
-      id="email"
-      placeholder="example@email.com"
-      class="bg-white"
-      v-model="email"
-    />
-    <label for="username">Username</label>
-    <input type="text" id="username" placeholder="johnDoe" class="bg-white" v-model="username" />
-    <label for="password">Password</label>
-    <input
-      type="password"
-      id="password"
-      minlength="8"
-      placeholder="Password ..."
-      class="bg-white"
-      v-model="password"
-    />
-    <label for="password">Confirm Password</label>
-    <input
-      type="password"
-      id="password"
-      minlength="8"
-      placeholder="Confirm Password"
-      class="bg-white"
-      v-model="verifiedPassword"
-    />
-    <div class="flex justify-between">
-      <button @click="createUser" class="bg-blue-300 cursor-pointer px-2 py rounded">
-        Register
-      </button>
-      <button @click="cancelForm" class="bg-blue-300 cursor-pointer px-2 py rounded">Cancel</button>
+  <div>
+    <div class="flex flex-col w-80 gap-2" v-if="isLogin">
+      <label for="email">Email</label>
+      <input
+        type="email"
+        id="email"
+        placeholder="example@email.com"
+        class="bg-white"
+        v-model="email"
+      />
+      <label for="username">Username</label>
+      <input type="text" id="username" placeholder="johnDoe" class="bg-white" v-model="username" />
+      <label for="password">Password</label>
+      <input
+        type="password"
+        id="password"
+        minlength="8"
+        placeholder="Password ..."
+        class="bg-white"
+        v-model="password"
+      />
+      <label for="password">Confirm Password</label>
+      <input
+        type="password"
+        id="password"
+        minlength="8"
+        placeholder="Confirm Password"
+        class="bg-white"
+        v-model="verifiedPassword"
+      />
+      <div>
+        <p @click="toggleLogin" class="text-blue-700 underline cursor-pointer">
+          Don't have an account?
+        </p>
+      </div>
+      <div class="flex justify-between">
+        <button @click="createUser" class="bg-blue-300 cursor-pointer px-2 py rounded">
+          Register
+        </button>
+        <button @click="cancelForm" class="bg-blue-300 cursor-pointer px-2 py rounded">
+          Cancel
+        </button>
+      </div>
+    </div>
+    <div v-else>
+      Login in instead
+      <div>
+        <p @click="toggleLogin" class="text-blue-700 underline cursor-pointer">Log in instead?</p>
+      </div>
     </div>
   </div>
 </template>
