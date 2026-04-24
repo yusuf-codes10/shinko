f68141c26ee9e9ea96f20935843237baa5f51e62
Yusef Codes <youcefcopy@gmail.com>
2026-04-24 16:47:28 +0100

    auth middleware has been defined

--- server/middlewares/authMiddleWare.js
+++ server/middlewares/authMiddleWare.js
@@ -4,6 +4,15 @@ const authMw = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) return res.status(401).json({ msg: "No token provided!" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Internal Server Error" });
+  }
 };
 
 export default authMw;
