5b6f2bceeb697f7ea2b0867f759ed5987fd1e021
Yusef Codes <youcefcopy@gmail.com>
2026-05-02 16:09:31 +0100

    authStore

---
+++ client/src/stores/authStore.js
@@ -0,0 +1,17 @@
+// stores/useUserStore.js
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useUserStore = defineStore('user', () => {
+  const user = ref(null)
+
+  const setUser = (userData) => {
+    user.value = userData
+  }
+
+  const clearUser = () => {
+    user.value = null
+  }
+
+  return { user, setUser, clearUser }
+})
