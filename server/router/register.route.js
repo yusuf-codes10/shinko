b7c4ecc855d48d585aea88f275aafeebc2dc1c9e
Yusef Codes <youcefcopy@gmail.com>
2026-05-02 17:01:22 +0100

    server: register/me route

--- server/router/register.route.js
+++ server/router/register.route.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { handleUser, logUserIn } from "../controllers/registerController.js";
+import verifyToken from "../middlewares/verifyToken.js";
 
 const router = express.Router();
 
 router.post("/", handleUser);
 
 router.post("/login", logUserIn);
+
+router.get("/me", verifyToken, (req, res) => {
+  res.status(200).json({ user: req.user });
+});
 
 export default router;
