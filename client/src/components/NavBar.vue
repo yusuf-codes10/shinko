3b2742721e8ed1085caee6a7641f59be90e1e45a
Yusef Codes <youcefcopy@gmail.com>
2026-05-02 17:35:28 +0100

    conditioanl rendering username and log in button

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -3,13 +3,16 @@ import { useAuthStore } from '@/stores/authStore.js'
 import { storeToRefs } from 'pinia'
 
 const authStore = useAuthStore()
-const { user } = storeToRefs(authStore)
+const { user, isLoggedIn, logout } = storeToRefs(authStore)
 </script>
 
 <template>
   <nav class="flex justify-between items-center w-full">
     <h1 class="text-3xl">Shinko</h1>
-    <h2 v-if="authStore.isLoggedIn">{{ user.username }}</h2>
+    <div v-if="isLoggedIn" class="flex justify-between gap-4">
+      <h2>{{ user?.username }}</h2>
+      <button class="cursor-pointer" @click="logout">logout</button>
+    </div>
     <button v-else class="bg-blue-300 px-4 py-2 cursor-pointer rounded-2xl">Login</button>
   </nav>
 </template>
