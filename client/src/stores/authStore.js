c5456709910408b70cec0fb8d30f3995bdac72fc
Yusef Codes <youcefcopy@gmail.com>
2026-05-02 16:54:36 +0100

    updating the authStore

--- client/src/stores/authStore.js
+++ client/src/stores/authStore.js
@@ -1,17 +1,47 @@
-// stores/useUserStore.js
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+import api from '@/services/api.js'
 
-export const useUserStore = defineStore('user', () => {
+export const useAuthStore = defineStore('auth', () => {
+  // * state
   const user = ref(null)
 
-  const setUser = (userData) => {
-    user.value = userData
+  // * actions
+  const register = async (credentials) => {
+    await api.post('/auth/register', credentials)
   }
 
-  const clearUser = () => {
+  const login = async (credentials) => {
+    await api.post('/auth/login', credentials)
+    // user.value = res.data.user
+    await fetchUser()
+    // populate user after login
+  }
+
+  const logout = async () => {
+    await api.post('/auth/logout')
     user.value = null
   }
 
-  return { user, setUser, clearUser }
+  const fetchUser = async () => {
+    try {
+      const res = await api.get('/auth/me')
+      user.value = res.data.user
+    } catch {
+      user.value = null // cookie expired or invalid
+    }
+  }
+
+  // * getters
+  const isLoggedIn = computed(() => !!user.value)
+
+  return {
+    user,
+    isLoggedIn,
+
+    register,
+    login,
+    logout,
+    fetchUser,
+  }
 })
