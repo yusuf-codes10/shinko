d8d5dff31b793784ce2c0f21c436c840c2625195
Yusef Codes <youcefcopy@gmail.com>
2026-05-02 13:57:16 +0100

    useCreateProject, has been added

--- client/src/composables/useProjects.js
+++ client/src/composables/useProjects.js
@@ -22,4 +22,26 @@ export const fetchAllProjects = () => {
   }
 }
 
-export const createProject = () => {}
+export const useCreateProject = () => {
+  const loading = ref(false)
+  const error = ref(null)
+
+  const create = async (payload) => {
+    try {
+      loading.value = true
+      const { data } = await createProject(payload)
+      return data
+    } catch (err) {
+      error.value = err
+      console.log(error.value)
+    } finally {
+      loading.value = false
+    }
+  }
+
+  return {
+    create,
+    loading,
+    error,
+  }
+}
