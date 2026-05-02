034e10287b4a596f643f87e19799f7250f15a03f
Yusef Codes <youcefcopy@gmail.com>
2026-05-02 15:20:28 +0100

    using cookies for the auth mw

--- server/middlewares/authMiddleWare.js
+++ server/middlewares/authMiddleWare.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const authMw = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
+  const token = eq.cookies.token; // 👈 only change
 
   if (!token) return res.status(401).json({ msg: "No token provided!" });
 
