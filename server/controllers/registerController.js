57c60e72baafa827dfe30c3a1a4f494bcbdd6587
Yusef Codes <youcefcopy@gmail.com>
2026-05-02 15:57:03 +0100

    fixing the cookie in the log controller

--- server/controllers/registerController.js
+++ server/controllers/registerController.js
@@ -74,7 +74,14 @@ export const logUserIn = async (req, res) => {
       { expiresIn: "7d" },
     );
 
-    res.json({ token });
+    res.cookie("token", token, {
+      httpOnly: true, // JS can't access it (safer)
+      secure: false, // set true in production (HTTPS only)
+      sameSite: "lax",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
+    });
+
+    res.status(200).json({ msg: "logged in successfully" }); // ← no token in body
   } catch (error) {
     console.log(error);
     res.status(500).json({ msg: error.message });
