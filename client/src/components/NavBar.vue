a4be9a31fd693b03d76f819f086c7ff1da8e6c62
Yusef Codes <youcefcopy@gmail.com>
2026-05-02 17:39:33 +0100

    await for the logogut, it is not working

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,9 +1,16 @@
 <script setup>
 import { useAuthStore } from '@/stores/authStore.js'
 import { storeToRefs } from 'pinia'
+import { useRouter } from 'vue-router'
 
 const authStore = useAuthStore()
-const { user, isLoggedIn, logout } = storeToRefs(authStore)
+const router = useRouter()
+const { user, isLoggedIn } = storeToRefs(authStore)
+
+const handleLogout = async () => {
+  await authStore.logout()
+  router.push('/')
+}
 </script>
 
 <template>
@@ -11,7 +18,7 @@ const { user, isLoggedIn, logout } = storeToRefs(authStore)
     <h1 class="text-3xl">Shinko</h1>
     <div v-if="isLoggedIn" class="flex justify-between gap-4">
       <h2>{{ user?.username }}</h2>
-      <button class="cursor-pointer" @click="logout">logout</button>
+      <button class="cursor-pointer" @click="handleLogout.logout">logout</button>
     </div>
     <button v-else class="bg-blue-300 px-4 py-2 cursor-pointer rounded-2xl">Login</button>
   </nav>
