041d636825038a60e7944ae96c7513341505f0a5
Yusef Codes <youcefcopy@gmail.com>
2026-05-03 17:27:40 +0100

    using the modal correctly

--- client/src/views/ProjectsView.vue
+++ client/src/views/ProjectsView.vue
@@ -2,6 +2,7 @@
 import KanProject from '@/components/KanProject.vue'
 import { ref, onMounted } from 'vue'
 import { getAllProjects, createProject } from '@/services/projectService.js'
+import KanModal from '@/components/ui/KanModal.vue'
 
 const projects = ref([])
 const loading = ref(false)
@@ -42,15 +43,11 @@ const toggleModal = () => {
         +New Project
       </button>
     </header>
-    <teleport to="#modal" v-if="isModalOpen">
-      <div class="modal-bg" @click="toggleModal">
-        <div class="modal" @click.stop>
-          <label for="name">Name: </label>
-          <input type="text" id="name" placeholder="name.." v-model="newProjectName" />
-          <button @click="createNewProject">Submit</button>
-        </div>
-      </div>
-    </teleport>
+    <KanModal :isOpen="isModalOpen" @close="toggleModal">
+      <label for="name">Name: </label>
+      <input type="text" id="name" placeholder="name.." v-model="newProjectName" />
+      <button @click="createNewProject">Submit</button>
+    </KanModal>
     <div v-if="loading">
       <h2>Loading...</h2>
     </div>
@@ -66,40 +63,4 @@ const toggleModal = () => {
 </template>
 
 <style scoped>
-.modal-bg {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.6);
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  z-index: 1000;
-}
-
-.modal {
-  background: white;
-  padding: 20px;
-  border-radius: 12px;
-  width: 300px;
-  max-width: 90%;
-
-  box-shadow: 0 10px 25px rgba(0, 0, 0, 0.2);
-  animation: fadeIn 0.2s ease;
-}
-
-@keyframes fadeIn {
-  from {
-    opacity: 0;
-    transform: scale(0.95);
-  }
-  to {
-    opacity: 1;
-    transform: scale(1);
-  }
-}
 </style>
