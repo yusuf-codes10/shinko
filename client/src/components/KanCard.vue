c8cf141b59231d96909e72102ebce4afd43e576a
Yusef Codes <youcefcopy@gmail.com>
2026-05-03 14:43:07 +0100

    adding some broders

--- client/src/components/KanCard.vue
+++ client/src/components/KanCard.vue
@@ -10,9 +10,9 @@ const emit = defineEmits(['create'])
 </script>
 
 <template>
-  <div class="bg-bg-raised py-4 px-2">
-    <div class="border-bg-border border-2">
-      <header class="flex justify-between bg-bg-purple">
+  <div class="bg-bg-raised rounded-xl py-4 px-2">
+    <div class="border-bg-border rounded-xl border-2">
+      <header class="flex justify-between">
         <h2>{{ props.title }}</h2>
         <i @click="emit('create')" class="fa-solid fa-plus cursor-pointer"></i>
       </header>
