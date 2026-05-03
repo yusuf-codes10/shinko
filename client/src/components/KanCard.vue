d235999dd436c6e77eddf7c957627c15ea4e8f17
Yusef Codes <youcefcopy@gmail.com>
2026-05-03 14:39:28 +0100

    adding some colors

--- client/src/components/KanCard.vue
+++ client/src/components/KanCard.vue
@@ -10,12 +10,14 @@ const emit = defineEmits(['create'])
 </script>
 
 <template>
-  <div class="bg-green-700 py-4 px-2">
-    <header class="flex justify-between">
-      <h2>{{ props.title }}</h2>
-      <i @click="emit('create')" class="fa-solid fa-plus cursor-pointer"></i>
-    </header>
-    <slot></slot>
+  <div class="bg-bg-raised py-4 px-2">
+    <div class="border-bg-border border-2">
+      <header class="flex justify-between bg-bg-purple">
+        <h2>{{ props.title }}</h2>
+        <i @click="emit('create')" class="fa-solid fa-plus cursor-pointer"></i>
+      </header>
+      <slot></slot>
+    </div>
   </div>
 </template>
 
