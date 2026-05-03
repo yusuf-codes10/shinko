caed4e6730279d64968be60fa83629dea124c7e1
Yusef Codes <youcefcopy@gmail.com>
2026-05-03 16:21:59 +0100

    we have to pass the username in

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,11 +1,14 @@
 <script setup>
+import { useAuthStore } from '@/stores/authStore.js'
+
+const authStore = useAuthStore()
 </script>
 
 <template>
   <div class="flex flex-col justify-center items-center h-screen">
     <div class="flex flex-col justify-center items-center h-screen">
       <h1 class="text-6xl md:text-9xl my-2 text-extrabold">shinko</h1>
-      <RouterLink :to="{ name: 'Projects' }">
+      <RouterLink :to="{ name: 'Projects', params: { username: authStore.user?.username } }">
         <button class="text-red-500 bg-green-400 px-1 py-2 rounded-sm cursor-pointer">
           Create New Project &rarr;
         </button>
