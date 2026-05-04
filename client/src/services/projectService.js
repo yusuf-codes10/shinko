2ee34a1808d2ed377c3b43b95fe6524099058604
Yusef Codes <youcefcopy@gmail.com>
2026-05-04 17:49:51 +0100

    matching that in vue side

--- client/src/services/projectService.js
+++ client/src/services/projectService.js
@@ -3,3 +3,5 @@ import api from '@/services/api.js'
 export const getAllProjects = () => api.get('/api/projects')
 
 export const createProject = (payload) => api.post('/api/projects', payload)
+
+export const countCompletedTasksByProject = (id) => api.get(`/api/projects${id}`)
