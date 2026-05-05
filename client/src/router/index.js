52f14829f0e3b2177315369b2c21803528e657e9
Yusef Codes <youcefcopy@gmail.com>
2026-05-05 13:39:21 +0100

    fixed the non exisiting route

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -15,7 +15,7 @@ const routes = [
     beforeEnter: () => {
       const auth = useAuthStore()
       if (auth.isLoggedIn) {
-        return { name: 'DashboardHome' }
+        return { name: 'Home' }
       }
     },
   },
@@ -37,7 +37,7 @@ const routes = [
     beforeEnter: () => {
       const auth = useAuthStore()
       if (auth.isLoggedIn) {
-        return { name: 'DashboardHome' }
+        return { name: 'Home' }
       }
     },
   },
