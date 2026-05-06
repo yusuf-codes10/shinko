485426cf8cc6eb7edb6a29b2595fe456792b38bb
Yusef Codes <youcefcopy@gmail.com>
2026-05-06 13:42:06 +0100

    progress bar component

---
+++ client/src/components/ui/ProgressBar.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+  width: {
+    type: String,
+    default: '0' + 'px',
+  },
+})
+</script>
+
+<template>
+  <div class="w-full p-4">
+    <div :style="{ width: props.width }" class="bg-accent"></div>
+  </div>
+</template>
+
+<style scoped>
+</style>
