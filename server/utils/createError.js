c63ced12f5d67ec37d185efc4d5698e71fac71bd
Yusef Codes <youcefcopy@gmail.com>
2026-05-08 15:32:00 +0100

    we can use the createError and add a code args

--- server/utils/createError.js
+++ server/utils/createError.js
@@ -1,6 +1,8 @@
-const createError = (statusCode, message) => {
+const createError = (statusCode, message, code = "SERVER_ERROR") => {
+  // the code param is for the fronted ux, SERVER_ERROR is the default
   const error = new Error(message);
   error.status = statusCode;
+  error.code = code;
   return error;
 };
 
