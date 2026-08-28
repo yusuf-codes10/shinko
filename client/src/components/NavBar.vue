944c7df55d684bfc69e57344e1487889a17df665
Yusef Codes <youcefcopy@gmail.com>
2026-08-28 18:46:32 +0100

    clickable user profile

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -40,7 +40,7 @@ const handleLogout = async () => {
         <i v-else @click="themeStore.toggleTheme" class="fa-solid fa-sun cursor-pointer"></i>
       </div>
       <div v-if="isLoggedIn" class="flex justify-between gap-2">
-        <h2>{{ user?.username }}</h2>
+        <h2 class="cursor-pointer">{{ user?.username }}</h2>
         <button class="cursor-pointer" @click="handleLogout">logout</button>
       </div>
       <RouterLink v-else :to="{ name: 'Register' }">
