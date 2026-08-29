1ab9473bffeb3835175d0fe99e325137accf403e
Yusef Codes <youcefcopy@gmail.com>
2026-08-29 17:05:44 +0100

    server: handling when no name

--- server/controllers/category.controller.js
+++ server/controllers/category.controller.js
@@ -2,6 +2,12 @@ import pool from "../db/pool.js";
 
 export const createCategory = async (req, res, next) => {
   const { name } = req.body;
+
+  if (!name) {
+    const error = new Error("Name and description are required!");
+    error.status = 400;
+    return next(error);
+  }
   try {
     const { rows } = await pool.query(
       "INSERT INTO category VALUE ($1) RETURNING *",
