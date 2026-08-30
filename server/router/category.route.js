d728d477dad73be7463d0abc113d415472a6e388
Yusef Codes <youcefcopy@gmail.com>
2026-08-30 13:26:37 +0100

    client: delete router added

--- server/router/category.route.js
+++ server/router/category.route.js
@@ -3,6 +3,7 @@ import authMw from "../middlewares/authMiddleWare.js";
 import {
   createCategory,
   getCategories,
+  deleteCategory,
 } from "../controllers/category.controller.js";
 
 const router = express.Router();
@@ -12,5 +13,8 @@ router.post("/", authMw, createCategory);
 
 // get all categories
 router.get("/", authMw, getCategories);
+
+// deelte a category
+router.delete("/:id", authMw, deleteCategory);
 
 export default router;
